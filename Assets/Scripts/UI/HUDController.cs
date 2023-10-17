99cf5d2c0726e68a136344684664131f131565ea
julabrego <abrego.jul@gmail.com>
2023-10-17 12:15:33 -0300

    HUD de vidas funcionando

--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -6,9 +6,56 @@ using TMPro;
 public class HUDController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI coinsText;
+    [SerializeField] GameObject liveIcon;
+    [SerializeField] GameObject liveIconsContainer;
 
+    public void UpdateLifeIconsHUD(int lives) {
+        if (isLifesContainerEmpty())
+        {
+            loadLifesContainer(lives);
+            return;
+        }
+
+        if(liveIconsQuantity() > lives)
+        {
+            removeLastLiveIcon();
+        }
+        else
+        {
+            createLiveIcon();
+        }
+    }
     public void UpdateCoinsText(string coins)
     {
         coinsText.text = "Coins: " + coins;
+    }
+
+    private bool isLifesContainerEmpty()
+    {
+        return liveIconsContainer.transform.childCount == 0;
+    }
+
+    private void loadLifesContainer(int iconsQuantity)
+    {
+        for(int i = 0; i < iconsQuantity; i++)
+        {
+            Instantiate(liveIcon, liveIconsContainer.transform);
+        }
+    }
+
+    private int liveIconsQuantity()
+    {
+        return liveIconsContainer.transform.childCount;
+    }
+
+    private void removeLastLiveIcon()
+    {
+        Transform container = liveIconsContainer.transform;
+        GameObject.Destroy(container.GetChild(container.childCount - 1).gameObject);
+    }
+
+    private void createLiveIcon()
+    {
+        Instantiate(liveIcon, liveIconsContainer.transform);
     }
 }
