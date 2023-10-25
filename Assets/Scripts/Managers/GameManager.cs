4dd3d05dcb5ee10b8a05aebe78f823b2323e4100
julabrego <abrego.jul@gmail.com>
2023-10-25 14:43:02 -0300

    Se crea GameManagementSingleton

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private int score;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int _score)
+    {
+        score += _score;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+}
