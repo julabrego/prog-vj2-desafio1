9d3253e32781015bfbc9177920a34d9de0c0a573
julabrego <abrego.jul@gmail.com>
2023-11-06 11:26:32 -0300

    se agregan invincible frames cuando se recibe daño

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,22 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] UnityEvent<int> OnPlayerReceivesDamage;
+    [SerializeField]
+    [Range(0, 5)]
+    [Tooltip("Tiempo en segundos en que el presonaje permanece invencible tras sufrir daño")]
+    private float invincibilityDuration = 2;
+
+    [SerializeField]
+    [Range(0, 1)]
+    [Tooltip("Delta time para manejar velocidad de invincible frames (parpadeo)")]
+    private float invincibilityDeltaTime = 0.12f;
+
     private Game game;
     private SpriteRenderer mySpriteRenderer;
     private CircleCollider2D myCollider;
 
-    [SerializeField] UnityEvent<int> OnPlayerReceivesDamage;
+    private bool isInvincible = false;
 
     private void Start()
     {
@@ -26,13 +37,39 @@ public class Player : MonoBehaviour
 
     public void ReceiveDamage(int points)
     {
-        OnPlayerReceivesDamage.Invoke(-points);
+        if (!isInvincible)
+        {
+            OnPlayerReceivesDamage.Invoke(-points);
+            if(game.Playing) StartCoroutine(BecomeTemporarilyInvincible());
+        }
     }
 
     private void makeInvisible()
     {
         myCollider.enabled = false;
         mySpriteRenderer.enabled = false;
+    }
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        isInvincible = true;
+        Debug.Log("Invincible");
+
+        for (float i = 0; i < invincibilityDuration; i += invincibilityDeltaTime)
+        {
+            yield return new WaitForSeconds(invincibilityDeltaTime);
+            if (mySpriteRenderer.enabled)
+            {
+                mySpriteRenderer.enabled = false;
+            }
+            else
+            {
+                mySpriteRenderer.enabled = true;
+            }
+        }
+
+        isInvincible = false;
+        mySpriteRenderer.enabled = true;
     }
 
 }
